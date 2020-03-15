d32110a41b9ca511bfba4bc519c9aff96e42964d
White_KJ <53563618+CwhiteKJ@users.noreply.github.com>
2020-03-15 21:05:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function getPlayers(ip) {
     return new Promise(function (resolve, reject) {
         request(`http://${ip}/`, function (err, response, body) {
             if (err) {
+                console.log("Server does not exist or is offline.")
                 return reject(err)
             } else {
                 const $ = cheerio.load(body)
@@ -16,4 +17,55 @@ function getPlayers(ip) {
     })
 }
 
+function getRes_running(ip) {
+    return new Promise(function (resolve, reject) {
+        request(`http://${ip}/`, function (err, response, body) {
+            if (err) {
+                console.log("Server does not exist or is offline.")
+                return reject(err)
+            } else {
+                const $ = cheerio.load(body)
+                let players = $(".text-value") 
+                var result = players[1].children[0].data;
+                resolve(result)
+            }
+        })
+    })
+}
+
+function getRes_load(ip) {
+    return new Promise(function (resolve, reject) {
+        request(`http://${ip}/`, function (err, response, body) {
+            if (err) {
+                console.log("Server does not exist or is offline.")
+                return reject(err)
+            } else {
+                const $ = cheerio.load(body)
+                let players = $(".text-value") 
+                var result = players[2].children[0].data;
+                resolve(result)
+            }
+        })
+    })
+}
+
+function getlatency(ip) {
+    return new Promise(function (resolve, reject) {
+        request(`http://${ip}/`, function (err, response, body) {
+            if (err) {
+                console.log("Server does not exist or is offline.")
+                return reject(err)
+            } else {
+                const $ = cheerio.load(body)
+                let players = $(".text-value") 
+                var result = players[3].children[0].data;
+                resolve(result)
+            }
+        })
+    })
+}
+
 exports.getPlayers = getPlayers
+exports.getRes_running = getRes_running
+exports.getRes_load = getRes_load
+exports.getlatency = getlatency
