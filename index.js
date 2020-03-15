85882966a9a488cd14160a27959b7192ffffa9d8
White_KJ <53563618+CwhiteKJ@users.noreply.github.com>
2020-03-15 20:23:53 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const request = require('request');
 const cheerio = require('cheerio');
 
 function getPlayers(ip) {
-    return new Promise(function() {    
+    return new Promise(function (resolve, reject) {
         request(`http://${ip}/`, function (err, response, body) {
-            const $ = cheerio.load(body)
-            let players = $(".text-value") 
-            var result = players[0].children[0].data;
-            if (err) return console.log(err)
-            console.log(result)
+            if (err) {
+                return reject(err)
+            } else {
+                const $ = cheerio.load(body)
+                let players = $(".text-value") 
+                var result = players[0].children[0].data;
+                resolve(result)
+            }
         })
     })
 }
